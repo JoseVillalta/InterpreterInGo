9535094ec09c089c80e3d0a236a24621f3d1de57
Jose Villalta <jovilll@amazon.com>
2019-09-05 09:23:22 -0700

    first commit. Lexer CH1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Monkey Interpreter
+
+## Overview
+Go code from Thorsten Ball's book
+https://www.amazon.com/dp/3982016118
---
+++ main.go
@@ -0,0 +1,7 @@
+package MonkeyInterpreter
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, this is my version of monkey")
+}
