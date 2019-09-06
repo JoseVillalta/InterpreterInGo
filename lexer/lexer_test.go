cf275aff2882bf41531102c9622e9767b65b2d7c
Jose Villalta <jovilll@amazon.com>
2019-09-06 16:46:15 -0700

    Adding more operators

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -47,7 +47,15 @@ func TestLexer_NextToken(t *testing.T){
     x + y;
     };
     
-    let result = add(five, ten);`
+    let result = add(five, ten);
+    !-/*5;
+    5 < 10 > 5;
+    
+    if (5 < 10) {
+     return true;
+     } else {
+     return false;
+     }`
 
 	tests := []struct {
 		expectedType token.TokenType
@@ -89,6 +97,36 @@ func TestLexer_NextToken(t *testing.T){
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+
 	}
 
 	l := New(input)
@@ -105,6 +143,46 @@ func TestLexer_NextToken(t *testing.T){
 				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
+}
+
+func TestTwoCharTokens(t *testing.T){
+	input := `if(var == 10){}
+     if(result != 5)`
+
+   tests := []struct {
+		expectedType token.TokenType
+		expectedLiteral string
+	}{
+	   {token.IF, "if"},
+	   {token.LPAREN, "("},
+	   {token.IDENT, "var"},
+	   {token.EQ, "=="},
+	   {token.INT, "10"},
+	   {token.RPAREN, ")"},
+	   {token.LBRACE, "{"},
+	   {token.RBRACE, "}"},
+	   {token.IF, "if"},
+	   {token.LPAREN, "("},
+	   {token.IDENT, "result"},
+	   {token.NOT_EQ, "!="},
+	   {token.INT, "5"},
+   }
+
+	l := New(input)
+	for i, tt := range tests{
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected = %q, got = %q",
+				i, tt.expectedType, tok.Type )
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - tokenliteral wrong. expected = %q, got = %q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+
 
 
 }
